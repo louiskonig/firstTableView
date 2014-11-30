c91221707f9f29150ec1fa6c80d457ccf89f94bb
louiskonig <louiskonig@gmail.com>
2014-11-30 17:15:36 -0500

    my first table view
    directions followed from website:
    http://www.weheartswift.com/how-to-make-a-simple-table-view-with-ios-8-a
    nd-swift/

---
+++ ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  firstTableView
+//
+//  Created by Louis Konig on 11/29/14.
+//  Copyright (c) 2014 Louis Konig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+  
+    var items: [String] = ["We", "Heart", "Swift"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        cell.textLabel.text = self.items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("You selected cell #\(indexPath.row)!")
+    }
+}
